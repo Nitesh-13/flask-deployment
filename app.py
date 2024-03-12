dd72e735a70148c1e992dd9d1450916e4a583a08
Nitesh-13 <niteshmali132002@gmail.com>
2024-03-12 22:26:04 +0530

    added extra route for file testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__, template_folder="templates")
 
@@ -6,6 +6,12 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/test")
+def test():
+    with open("test.txt") as f:
+        f.write("Hello world")
+    return send_file("test.txt")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
